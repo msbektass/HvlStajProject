4c04d59e7f6a17f64cb36ddc6653002d12a016a9
sulumanski <suleymanbektas203@gmail.com>
2026-07-23 16:06:28 +0300

    Global exception handler added

---
+++ src/main/java/com/example/hvlstajproject/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.hvlstajproject.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends RuntimeException {
+    @ExceptionHandler(DuplicateTcNoException.class)
+    public ResponseEntity<Map<String, String>> handleDuplicateTcNoException(DuplicateTcNoException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("message", e.getMessage()));
+    }
+    @ExceptionHandler(PatientNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlePatientNotFoundException(PatientNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("message", e.getMessage()));
+    }
+}
