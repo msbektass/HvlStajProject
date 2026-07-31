d54afdeb3b8dfa3969330f91461f70ab1d261409
Muhammet Süleyman Bektaş <suleymanbektas203@gmail.com>
2026-07-31 09:46:57 +0300

    Refactor - @Data yerine @Setter ve @Getter kullanıldı
    Replaced @Data annotation with @Setter and @Getter for more granular control over generated methods.

--- src/main/java/com/example/hvlstajproject/appointment/dto/AppointmentStatusChangeDTO.java
+++ src/main/java/com/example/hvlstajproject/appointment/dto/AppointmentStatusChangeDTO.java
@@ -6,7 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+@Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AppointmentStatusChangeDTO {
